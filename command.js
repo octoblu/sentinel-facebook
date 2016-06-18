e17a5202d77a7316a9d832da1e8689b186b5d231
sqrt <sqrt@octoblu.com>
2016-06-17 17:11:43 -0700

    v1.0.4 Log base64 image on failure

--- command.js
+++ command.js
@@ -3,7 +3,8 @@
 var system = require('system');
 var casper = require('casper').create({
   onError: (function(error){
-    casper.echo("failure due to error: " + error)
+    console.log("failure due to error: " + error)
+    console.log(this.echo(casper.captureBase64('png')))
     casper.exit(1)
   })
 });
@@ -12,8 +13,12 @@ var casper = require('casper').create({
 var FACEBOOK_USERNAME = system.env.FACEBOOK_USERNAME;
 var FACEBOOK_PASSWORD = system.env.FACEBOOK_PASSWORD;
 
-if(!FACEBOOK_USERNAME || !FACEBOOK_PASSWORD) casper.die('Missing required env: FACEBOOK_USERNAME or FACEBOOK_PASSWORD')
+if(!FACEBOOK_USERNAME || !FACEBOOK_PASSWORD) {
+  console.log('Missing required env: FACEBOOK_USERNAME or FACEBOOK_PASSWORD')
+  this.exit(1)
+}
 
+casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36');
 casper.start('https://app.octoblu.com/');
 
 casper.waitForText("Facebook")
@@ -36,7 +41,9 @@ casper.waitForText("dashboard", function(){
   this.echo("success");
   this.exit()
 }, function(){
-  this.die("failure")
+  console.log("failure")
+  console.log(this.echo(casper.captureBase64('png')))
+  this.exit(1)
 })
 
 casper.run();
