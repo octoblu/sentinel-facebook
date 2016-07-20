c661d0131ee263174bb51a23bbc02cfd2ebe050c
miql / royvandewater <michael@octoblu.com , r@octoblu.com>
2016-07-20 11:39:21 -0700

    v1.0.6 Update to write image to fs

--- command.js
+++ command.js
@@ -1,15 +1,9 @@
 #!/usr/bin/env casperjs
 
-var system = require('system');
-var casper = require('casper').create({
-  waitTimeout: (10 * 1000),
-  onError: (function(error){
-    console.log("failure due to error: " + error)
-    console.log(this.echo(casper.captureBase64('png')))
-    casper.exit(1)
-  })
-});
-
+var system  = require('system');
+var helpers = require('./helpers');
+var Casper  = require('casper');
+var casper  = helpers.buildCasper(Casper, 'sentinel-twitter');
 
 var FACEBOOK_USERNAME = system.env.FACEBOOK_USERNAME;
 var FACEBOOK_PASSWORD = system.env.FACEBOOK_PASSWORD;
@@ -19,32 +13,27 @@ if(!FACEBOOK_USERNAME || !FACEBOOK_PASSWORD) {
   this.exit(1)
 }
 
-casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36');
-casper.start('https://app.octoblu.com/');
-
-casper.waitForText("Facebook")
-
-casper.then(function() {
-  this.click('.auth__button--facebook');
+helpers.thenWithErrors(casper, function() {
+  casper.click('.auth__button--facebook');
 })
 
-casper.waitForText("Log into Facebook")
+casper.waitForSelector("#login_form");
 
-casper.then(function(){
-  this.fill('#login_form', {
+helpers.thenWithErrors(casper, function() {
+  casper.fill('#login_form', {
     'email': FACEBOOK_USERNAME,
     'pass': FACEBOOK_PASSWORD
   })
-  this.click("#loginbutton")
+  casper.click("#loginbutton")
 })
 
-casper.waitForText("dashboard", function(){
-  this.echo("success");
-  this.exit()
-}, function(){
-  console.log("failure")
-  console.log(this.echo(casper.captureBase64('png')))
-  this.exit(1)
+helpers.assertOnOctobluDashboard(casper);
+helpers.thenWithErrors(casper, function(){
+  helpers.logout(casper);
+});
+helpers.thenWithErrors(casper, function(){
+  casper.echo("success");
+  casper.exit(0);
 })
 
 casper.run();
---
+++ helpers.js
@@ -0,0 +1,77 @@
+var filename = "image";
+
+var assertOnOctobluDashboard = function(casper){
+  return casper.waitForSelector(".TabBar-title", function(){
+    return;
+  }, function(){
+    console.log("failure to load dashboard")
+    casper.capture('./images/' + randomFilename());
+    casper.exit(1)
+    casper.waitForSelector('.endo-of-the-world', (function(){}), (function(){}), 30000);
+  });
+};
+
+var buildCasper = function(Casper, _filename){
+  filename = _filename;
+  var casper = Casper.create({
+    waitTimeout: (10 * 1000),
+    onError: (function(error){
+      console.log("failure due to error: " + error)
+      casper.capture('./images/' + randomFilename());
+      casper.exit(1)
+      casper.waitForSelector('.endo-of-the-world', (function(){}), (function(){}), 30000);
+    })
+  });
+
+  casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36');
+  casper.start('https://app.octoblu.com/');
+
+  casper.waitForSelector(".auth.login");
+  casper.on('error', function(error){
+    console.log("failure due to casper error: " + error)
+    casper.capture('./images/' + randomFilename());
+    casper.exit(1)
+    casper.waitForSelector('.endo-of-the-world', (function(){}), (function(){}), 30000);
+  });
+  return casper;
+};
+
+var logout = function(casper){
+  casper.waitForSelector('.TopBar-settings');
+  casper.click(".TopBar-settings");
+
+  casper.waitForSelector('a[aria-label="Sign Out"]');
+  casper.click('a[aria-label="Sign Out"]');
+
+  casper.waitForSelector(".auth.login");
+}
+
+var randomFilename = function(){
+  var a = Math.random();
+  return filename+"-"+parseInt(a * 1000)+".png";
+}
+
+var reportErrors = function(casper, f) {
+  try {
+    return f();
+  } catch (e) {
+    casper.echo("failure in thenWithErrors: " + e)
+    casper.capture('./images/' + randomFilename());
+    casper.exit(1)
+    casper.waitForSelector('.endo-of-the-world', (function(){}), (function(){}), 30000);
+  }
+};
+
+var thenWithErrors = function(casper, f){
+  return casper.then(function() {
+    return reportErrors(casper, f);
+  });
+};
+
+module.exports = {
+  assertOnOctobluDashboard: assertOnOctobluDashboard,
+  buildCasper: buildCasper,
+  logout: logout,
+  reportErrors: reportErrors,
+  thenWithErrors: thenWithErrors
+};
